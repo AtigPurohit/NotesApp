60deee7a4b15e00295678fb70c73626aa9febebb
Atig Purohit <itsthechamp0074@gmail.com>
2021-01-18 09:54:40 +0530

    Notes App

---
+++ app/src/main/java/com/example/notesapp/Note.kt
@@ -0,0 +1,9 @@
+package com.example.notesapp
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "notes_table")
+class Note(@ColumnInfo(name = "text")val text: String){
+    @PrimaryKey(autoGenerate = true) var id = 0
+}
